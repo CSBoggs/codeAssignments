f17ad3a0d88c7882593e3aef1f1e5d79a72b4386
CSBoggs <cboggs.dev@gmail.com>
2021-07-09 19:56:50 -0600

    modified week 9a with arrays and for loop

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-// var userAge = 17;
-// var is_subscribed = false;
+var userAge = [16, 19, 18, 18, 22, 21, 15];
+var is_subscribed = [false, true, true, true, false, false, true];
+var arrayLength = is_subscribed.length;
 
-// if (is_subscribed == false && userAge < 18) {
-//     console.log("The user is younger than 18 and is not subscribed");
-// } else if (is_subscribed == true && userAge < 18) {
-//     console.log("The user is younger than 18 and is subscribed");
-// } else if (is_subscribed == false && userAge > 18) {
-//     console.log("The user is older than 18 and is not subscribed");
-// } else if (is_subscribed == true && userAge > 18) {
-//     console.log("The user is older than 18 and is subscribed");
-// } else {
-//     console.log("The user does not meet specifications");
-// }
+for (var counter = 0; counter < arrayLength; counter++) {
 
-var userAge = 6;
-var is_subscribed = false;
-
-if (is_subscribed) {
-    if (userAge < 18) {
-        console.log("The user is subscribed and younger than 18");
-    } else if (userAge >= 18) {
-        console.log("The user is subscribed and older than 18");
+    if (is_subscribed[counter]) {
+        if (userAge[counter] < 18) {
+            console.log("The user is subscribed and younger than 18");
+        } else if (userAge[counter] >= 18) {
+            console.log("The user is subscribed and older than 18");
+        }
+    } else {
+        if (userAge[counter] < 18) {
+            console.log("The user is not subscribed and younger than 18");
+        } else if (userAge[counter] >= 18) {
+            console.log("The user is not subscribed and older than 18");
+        }
     }
-} else if (!is_subscribed) {
-    if (userAge < 18) {
-        console.log("The user is not subscribed and younger than 18");
-    } else if (userAge >= 18) {
-        console.log("The user is not subscribed and older than 18");
-    }
-} else {
-    console.log("The user does not meet specifications");
+    console.log();
 }
